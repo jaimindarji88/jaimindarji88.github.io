fe606376834a8ead7b895bf095af6bfb103f563d
jaimindarji <jaimin.d.88@gmail.com>
2015-12-06 13:12:35 -0500

    added images

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var backgrounds = new Array(
+	'url(/images/Cosmos01.jpg) center no-repeat',
+	'url(/images/Cosmos02.jpg) center no-repeat',
+	'url(/images/Cosmos03.jpg) center no-repeat',
+	'url(/images/Cosmos04.jpg) center no-repeat',
+	'url(/images/Cosmos05.jpg) center no-repeat',
+	'url(/images/Cosmos06.jpg) center no-repeat'
+);
+var current = 0;
+
+
+
+
+$(document).ready(function() {
+	var backgroundDiv = $('background-cover');
+
+	function nextBackground() {
+		backgroundDiv.css(
+			'background',
+			backgrounds[current = ++current % backgrounds.length]
+		);
+
+		setTimeout(nextBackground, 10000);
+	}
+
+	setTimeout(nextBackground, 1000);
+	backgroundDiv.css('background', backgrounds[0]);
+}); 
